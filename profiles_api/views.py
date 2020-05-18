78d13194f9da1096f77db36642ca8af60cbb1768
shayanBING <shafieazad.shayan@gmail.com>
2020-05-18 17:32:22 +0430

    post method with serializer

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializer
+
 
 class HelloApiView(APIView):
     """Test Api View"""
+    serializer_class = serializer.HelloSerializer
+
     def get(self,request,format=None):
          """Return list of ApiView Features"""
          an_apiview=[
@@ -13,3 +18,17 @@ class HelloApiView(APIView):
          ]
 
          return Response({'message':"Hello!","an_apiview":an_apiview})
+
+    def post(self,request):
+        """Create hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        #if serializer value is valid do our logic
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = 'Hello {}'.format(name)
+            return Response({'message':message})
+        else:
+            # error http 400 bad request
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
