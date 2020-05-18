b1b10f3db62716426b22b62d74e9c47dd61f8be0
shayanBING <shafieazad.shayan@gmail.com>
2020-05-18 16:45:57 +0430

    Firs Api View (get) Function

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test Api View"""
+    def get(self,request,format=None):
+         """Return list of ApiView Features"""
+         an_apiview=[
+         'Use Http method as function(get,post,patch,put,delete) ',
+         'is similar to traditional Django View',
+         'gives you the most control over your application logic',
+         'its map to url'
+         ]
+
+         return Response({'message':"Hello!","an_apiview":an_apiview})
